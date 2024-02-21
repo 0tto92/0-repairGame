81f51ac9131fbf3aa1cefb85cfb096d6e80a0873
0tto92 <109680629+0tto92@users.noreply.github.com>
2024-02-21 14:49:34 +0200

    Add files via upload

---
+++ src/cl_main.lua
@@ -0,0 +1,28 @@
+local gameStarted = false
+
+local function repairGame(difficulty, text)
+    if gameStarted then return end
+    gameStarted = true
+
+    SendNUIMessage({
+        type = 'repairGame',
+        difficulty = difficulty,
+        text = text
+    })
+
+    SetNuiFocus(true, true)
+    return percentage
+end exports('repairGame', repairGame)
+
+RegisterCommand('minigame', function()
+    repairGame('easy', 'HOMO')
+end)
+
+RegisterNUICallback('repairGameFinished', function(data, cb)
+    if not gameStarted then print('nui devtools') return false end
+
+    gameStarted = false
+    percentage = data.percentage
+    SetNuiFocus(false, false)
+    cb()
+end)
