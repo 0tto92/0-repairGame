22599cdf785b91c87d2da4df4cddffe91873bc06
0tto92 <109680629+0tto92@users.noreply.github.com>
2024-02-21 14:50:04 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    '**/cl_*.lua'
+}
+
+ui_page "web/index.html"
+
+files {
+    '**/*.html',
+    '**/**/*.css',
+    '**/**/*.js'
+}
